28cee9997a6d646699a4f075101737d718a5e62e
Uliana Afonina <afonina.uliana@otr.ru>
2022-03-01 13:06:59 +0300

    Hibernate. @GenerationValue стратегия генерации PrimaryKey

--- src/main/java/com/ulianaafonina/spring/hibernate_test/Test1.java
+++ src/main/java/com/ulianaafonina/spring/hibernate_test/Test1.java
@@ -8,19 +8,42 @@ import org.pmw.tinylog.Logger;
 
 public class Test1 {
     public static void main(String[] args) { // factory нужно закрывать в любом случае, даже если вылезет exception, поэтому используем try with resources
-        try (SessionFactory factory = new Configuration() //можем переиспользовать factory потом во всём приложении
-                .configure("hibernate.cfg.xml") //на основании какого конфигурационного файла будет строиться сессия с БД. Файл в пакете resources
-                .addAnnotatedClass(Employee.class) //Employee.class имеет аннотации для работы с БД
-                .buildSessionFactory()) {
+        Test1 test1 = new Test1();
+        Employee employee1 = new Employee("Александр", "Смирнов", "Sales", 120);//id будет генерироваться автоматически
+        test1.insertRowInEmployees(employee1);
+//        test1.updateRowInEmployees(employee1);
+    }
+
+    private void insertRowInEmployees(Employee employee) {
+        try (SessionFactory factory = getFactory()) {
 
             Session session = factory.getCurrentSession(); //подключение к БД, живёт недолго (ровно чтоб получить данные из БД), потом закрываем её
 
-            Employee employee = new Employee("Михаил", "Иванов", "HR", 100);//id будет генерироваться автоматически
+//            Employee employee = new Employee(name, surname, department, salary);//id будет генерироваться автоматически
             session.beginTransaction(); //открываем транзакцию
             session.save(employee); //добавили в БД объект employee
             session.getTransaction().commit(); //закрыли транзакцию
             Logger.info("Запись успешно добавлена в таблицу employees.");
         }
+    }
 
+//    private void updateRowInEmployees(Employee employee) {
+//        try (SessionFactory factory = getFactory()) {
+//
+//            Session session = factory.getCurrentSession(); //подключение к БД, живёт недолго (ровно чтоб получить данные из БД), потом закрываем её
+//
+////            Employee employee = new Employee(name, surname, department, salary);//id будет генерироваться автоматически
+//            session.beginTransaction(); //открываем транзакцию
+//            session.update(employee); //добавили в БД объект employee
+//            session.getTransaction().commit(); //закрыли транзакцию
+//            Logger.info("Запись успешно добавлена в таблицу employees.");
+//        }
+//    }
+
+    private SessionFactory getFactory() {
+        return new Configuration() //можем переиспользовать factory потом во всём приложении
+                .configure("hibernate.cfg.xml") //на основании какого конфигурационного файла будет строиться сессия с БД. Файл в пакете resources
+                .addAnnotatedClass(Employee.class) //Employee.class имеет аннотации для работы с БД
+                .buildSessionFactory();
     }
 }
