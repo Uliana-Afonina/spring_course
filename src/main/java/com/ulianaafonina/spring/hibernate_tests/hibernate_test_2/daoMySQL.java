b9061dc1fad929a06b0f3d93dbca04d9d63daf02
Uliana Afonina <afonina.uliana@otr.ru>
2022-03-10 17:27:57 +0300

    Revert "Hibernate. Wrong"
    This reverts commit a1cb63bd

--- src/main/java/com/ulianaafonina/spring/hibernate_tests/hibernate_test_2/daoMySQL.java
+++ src/main/java/com/ulianaafonina/spring/hibernate_tests/hibernate_test_2/daoMySQL.java
@@ -78,24 +78,27 @@ public class daoMySQL {
             session.delete(employee);
             System.out.println(employee.getEmpDetail());
 
-            session.getTransaction().commit(); //закрыли ткарта военных действий на украине 2022ранзакцию
+            session.getTransaction().commit(); //закрыли транзакцию
 
             Logger.info("Запись успешно изменена.");
         }
     }
 
-    public Em getEmployeeByDetails(Detail detail, int id) {
+    public void insertRowInEmployeesAndDetails(Employee employee, Detail detail) {
         try (SessionFactory factory = getFactory();
              Session session = factory.getCurrentSession();) { // factory нужно закрывать в любом случае, даже если вылезет exception, поэтому используем try with resources
 
-            detail = session.get(Detail.class, id);
+            //подключение к БД, живёт недолго (ровно чтоб получить данные из БД), потом закрываем её
+
+            employee.setEmpDetail(detail); //работнику добавить детали
+            detail.setEmployee(employee); // деталям добавить работника
 
             session.beginTransaction(); //открываем транзакцию
+            session.save(detail); //добавили в БД объект employee (details добавится тоже благодаря cascade)
             session.getTransaction().commit(); //закрыли транзакцию
 
 //            System.out.println(employee);
             Logger.info("Запись успешно добавлена в таблицу employees.");
         }
     }
-
 }
